a36698c57613b048ed6675d2cb854b293cb78483
lunglee <lunglungleelee@hotmail.com>
2016-05-16 20:24:44 +0800

    g

---
+++ CC-Design/src/CC-Design/ViewModels/Manage/FactorViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CC_Design.ViewModels.Manage
+{
+    public class FactorViewModel
+    {
+        public string Purpose { get; set; }
+    }
+}
